62b0042395ca267b6c0c82844254e2963da933ef
Tonusha <Sidtonya@yandex.ru>
2015-02-11 09:21:42 +0300

    Rejected commit: Default

---
+++ MirukAntanina/src/Hw3/Task8.java
@@ -0,0 +1,26 @@
+package Hw3;
+
+/**
+ * Created by miruk on 2/11/15.
+ */
+public class Task8 {
+    public static void main(String[] args) {
+        double averageProd = 0;
+        double Prod = 1;
+        int n = 0;
+        int x = (int) (Math.random() * 20);
+        System.out.println(x);
+        while (x != 0) {
+            Prod *= x;
+            n++;
+            x = (int) (Math.random() * 20);
+            System.out.println(x);
+        }
+        if (n != 0) {
+            averageProd = Prod / n;
+        } else {
+            averageProd = 0;
+        }
+        System.out.println("Среднее: " + averageProd);
+    }
+}
