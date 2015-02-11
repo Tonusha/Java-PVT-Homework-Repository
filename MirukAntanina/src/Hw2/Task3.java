afc89da1cfd8d98b8d98cb26a80aa0c85a7b850e
Tonusha <Sidtonya@yandex.ru>
2015-02-11 09:14:30 +0300

    Rejected commit: Default

---
+++ MirukAntanina/src/Hw2/Task3.java
@@ -0,0 +1,15 @@
+package Hw2;
+
+/**
+ * Created by miruk on 2/11/15.
+ */
+public class Task3 {
+    public static void main(String[] arg) {
+        int num = 157;
+        if (num % 10 == 7) {
+            System.out.println("Последняя цифра числа " + num + " - 7.");
+        } else {
+            System.out.println("Последняя цифра числа " + num + " не 7.");
+        }
+    }
+}
