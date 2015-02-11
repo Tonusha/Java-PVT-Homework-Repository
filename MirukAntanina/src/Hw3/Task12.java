f6affe14b572de5acd1a81e09d1b839ca5477a48
Tonusha <Sidtonya@yandex.ru>
2015-02-11 09:41:38 +0300

    Rejected commit: Default

---
+++ MirukAntanina/src/Hw3/Task12.java
@@ -0,0 +1,26 @@
+package Hw3;
+
+/**
+ * Created by miruk on 2/11/15.
+ */
+public class Task12 {
+    public static void main(String[] args) {
+        int array[] = new int[20];
+        int maxIndex = 0;
+        System.out.print("Массив: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10 + 1);
+            System.out.print(array[i] + ", ");
+        }
+        int maxelem = array[0];
+        System.out.println("  ");
+        for (int i = 1; i < array.length; i++) {
+            if (maxelem < array[i]) {
+                maxelem = array[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("Максимальный индекс элемента: " + maxIndex);
+
+    }
+}
