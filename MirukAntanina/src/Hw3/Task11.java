1146e76b21c226f74987135f09546cc1704ce937
Tonusha <Sidtonya@yandex.ru>
2015-02-11 09:41:13 +0300

    Rejected commit: Default

---
+++ MirukAntanina/src/Hw3/Task11.java
@@ -0,0 +1,27 @@
+package Hw3;
+
+/**
+ * Created by miruk on 2/11/15.
+ */
+public class Task11 {
+    public static void main(String[] args) {
+        int array[] = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10 + 1);
+        }
+        int sum = 0;
+        int numbreak = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                System.out.println(array[i]);
+                sum += array[i];
+                numbreak += 1;
+            }
+            if (numbreak == 2) {
+                break;
+            }
+        }
+        System.out.println("Сумма первых n чисел, которые делятся на 3 равна: " + sum);
+
+    }
+}
