2a17306d0ecd40ab324dd1d07b6596bede8c64ac
Tonusha <Sidtonya@yandex.ru>
2015-02-12 12:01:12 +0300

    Task13

--- MirukAntanina/src/Hw3/Task13.java
+++ MirukAntanina/src/Hw3/Task13.java
@@ -8,14 +8,14 @@ public class Task13 {
         int k = 0;
         for (int i = 0; i <= size; i++) {
             for (int j = 0; j <= size; j++) {
-                array[i][j] = (int) (Math.random() *10);
+                array[i][j] = (int) (Math.random() * 10);
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
 
         for (int i = 0; i <= size / 2; i++) {
-            for (int j = i; j <= size-i-1; j++) {
+            for (int j = i; j <= size - i - 1; j++) {
                 int temp = array[i][j];
                 array[i][j] = array[size - k][i];
                 array[size - j][i] = array[size - i][size - j];
@@ -27,7 +27,7 @@ public class Task13 {
         System.out.println("Массив, повернутый по часовой стрелке: ");
         for (int i = 0; i <= size; i++) {
             for (int j = 0; j <= size; j++) {
-                System.out.print(array[i][j]+ " ");
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
