61af88ecf64996f9287b1f8791b44d932a0657b0
Tonusha <Sidtonya@yandex.ru>
2015-02-12 12:00:42 +0300

    Task13

--- MirukAntanina/src/Hw3/Task13.java
+++ MirukAntanina/src/Hw3/Task13.java
@@ -1,49 +1,33 @@
 package Hw3;
 
-import java.util.Scanner;
-
-/**
- * Created by miruk on 2/11/15.
- */
 public class Task13 {
     public static void main(String[] args) {
-        System.out.print("Введите размерность массива: ");
-        byte ind = 0;
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextByte()) {
-            ind = sc.nextByte();
-        }
-        int array[][] = new int[ind][ind];
+        int array[][] = new int[5][5];
         System.out.println("Массив: ");
         int size = array.length - 1;
         int k = 0;
-        String str;
         for (int i = 0; i <= size; i++) {
             for (int j = 0; j <= size; j++) {
-                array[i][j] = k++;
-                if (k < 10) str = "  ";
-                else str = " ";
-                System.out.print(array[i][j] + str);
+                array[i][j] = (int) (Math.random() *10);
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
 
         for (int i = 0; i <= size / 2; i++) {
-            for (k = i; k <= size - 1 - i; k++) {
-                int temp = array[i][k];//
-                array[i][k] = array[size - k][i];//последний элемент столбца
-                array[size - k][i] = array[size - i][size - k];
-                array[size - i][size - k] = array[k][size - i];
-                array[k][size - i] = temp;
+            for (int j = i; j <= size-i-1; j++) {
+                int temp = array[i][j];
+                array[i][j] = array[size - k][i];
+                array[size - j][i] = array[size - i][size - j];
+                array[size - i][size - j] = array[j][size - i];
+                array[j][size - i] = temp;
             }
         }
 
         System.out.println("Массив, повернутый по часовой стрелке: ");
         for (int i = 0; i <= size; i++) {
             for (int j = 0; j <= size; j++) {
-                if (array[i][j] < 10) str = "  ";
-                else str = " ";
-                System.out.print(array[i][j] + str);
+                System.out.print(array[i][j]+ " ");
             }
             System.out.println();
         }
