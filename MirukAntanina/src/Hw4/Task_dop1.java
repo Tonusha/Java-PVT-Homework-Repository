b260ba947d8879aa34781a9e5917affa14a04e4e
Tonusha <Sidtonya@yandex.ru>
2015-02-12 09:42:02 +0300

    Rejected commit: Default

--- MirukAntanina/src/Hw4/Task_dop1.java
+++ MirukAntanina/src/Hw4/Task_dop1.java
@@ -1,7 +1,26 @@
 package Hw4;
 
+import java.util.Scanner;
+
 /**
  * Created by miruk on 2/11/15.
  */
 public class Task_dop1 {
+    public static void main(String[] args) {
+        float number;
+        System.out.print("Введите дробное число: ");
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextFloat();
+        number *= 1000;
+        //System.out.println(number);
+        float first = number % 10;
+        float two = (number % 100-first)/10;
+        float three = (number % 1000-number % 100)/100;
+        //System.out.println(first + "  " + two + "  " + three);
+        int sum = (int)first + (int)two + (int)three;
+        System.out.println("Сумма первых трех цифр дробной части заданного положительного вещественного числа "+sum);
+    }
 }
+
+
+
