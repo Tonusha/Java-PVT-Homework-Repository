727a742ff452f6ffab68668450b9760d130283e7
Tonusha <Sidtonya@yandex.ru>
2015-02-12 12:07:47 +0300

    Task_dop3

--- MirukAntanina/src/Hw4/Task_dop3.java
+++ MirukAntanina/src/Hw4/Task_dop3.java
@@ -1,16 +1,16 @@
 package Hw4;
 
 /**
- Определить, какую наибольшую степень числа 100 можно вычислить, пользуясь типом int
+ * Определить, какую наибольшую степень числа 100 можно вычислить, пользуясь типом int
  */
 public class Task_dop3 {
     public static void main(String[] args) {
-        final int NUMBER = 100;
+        final int number = 100;
         int step = 0;
         int rezult = 0;
 
         while (rezult < Integer.MAX_VALUE) {
-            rezult = (int)Math.pow(NUMBER, step);
+            rezult = (int) Math.pow(number, step);
             step++;
         }
 
