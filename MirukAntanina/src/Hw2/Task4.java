4cdbb86cfeaf297304ea4e9f1ce84d6af0c69845
Tonusha <Sidtonya@yandex.ru>
2015-02-13 12:31:37 +0300

    Task_dop4

--- MirukAntanina/src/Hw2/Task4.java
+++ MirukAntanina/src/Hw2/Task4.java
@@ -2,15 +2,13 @@ package Hw2;
 
 public class Task4 {
     public static void main(String[] arg) {
-        int a = 2;
-        int b = 4;
-        int r = 6;
-        float c = a*a+b*b;
+        int a = 2, b = 4, r = 6;
+        float c = a * a + b * b;
         double d = Math.pow(c, 0.5) / 2;
         if (r >= d) {
-            System.out.println("Прямоугольное отверстие с размерами "+ a + "x" + b + " можно накрыть круглой картонкой радиусом " + r);
+            System.out.println("Прямоугольное отверстие с размерами " + a + "x" + b + " можно накрыть круглой картонкой радиусом " + r);
         } else {
-            System.out.println("Прямоугольное отверстие с размерами "+ a + "x" + b + " нельзя накрыть круглой картонкой радиусом " + r);
+            System.out.println("Прямоугольное отверстие с размерами " + a + "x" + b + " нельзя накрыть круглой картонкой радиусом " + r);
         }
     }
 }
