8ddfda3bc63fb74539151db412094803c651d2f7
Tonusha <Sidtonya@yandex.ru>
2015-02-13 12:29:45 +0300

    Task_dop4

--- MirukAntanina/src/Hw2/Task2.java
+++ MirukAntanina/src/Hw2/Task2.java
@@ -1,11 +1,13 @@
 package Hw2;
-
+import java.util.Scanner;
 /**
  * Created by miruk on 2/11/15.
  */
 public class Task2 {
     public static void main(String[] arg) {
-        int s = 86400*188 + 3600 + 60 + 9;
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
         int sec = s % 60;
         int m = (s - sec) / 60;
         int min = m % 60;
@@ -14,48 +16,7 @@ public class Task2 {
         int d = (h - hour) / 24;
         int day = d % 7;
         int w = (d - day) / 7;
-
-        String  ww, dd, hh,mm,ss;
-
-        if ((w%10==1)&&(w!=11)) {    ww = " неделя ";
-        }
-        else if ((
-                (w%10==2)||(w%10==3)||(w%10==4))&&((w!=12)||(w!=13)||(w!=14))) {
-            ww = " недели ";
-        }
-        else {ww = " недель ";}
-
-        if (d==1) {    dd = " день ";
-        }
-        else if ((d>=2)&&(d<=4)) {
-            dd = " дня ";
-        }
-        else {dd = " дней ";}
-
-        if ((hour==1)||(hour==21)) { hh = " час ";
-        }
-        else if (((hour>=2)&&(hour<=4))||(hour==22)||(hour==23)) {
-            hh = " часа ";
-        }
-        else {hh = " часов ";}
-
-        if ((min%10==1)&&(min!=11)) {    mm = " минута ";
-        }
-        else if ((
-                (min%10==2)||(min%10==3)||(min%10==4))&&((min!=12)||(min!=13)||(min!=14))) {
-            mm =  " минуты ";
-        }
-        else {mm =  " минут ";}
-
-        if ((sec%10==1)&&(sec!=11)) {    ss = " секунда ";
-        }
-        else if ((
-                (sec%10==2)||(sec%10==3)||(sec%10==4))&&((sec!=12)||(sec!=13)||(sec!=14))) {
-            ss =  " секунды ";
-        }
-        else {ss =  " секунд ";}
-
-        System.out.println(w + ww + day + dd + hour + hh + min + mm + sec + ss);
+        System.out.println(w + " weeks " + day + " days " + hour + " hours " + min + " minutes " + sec + " seconds");
     }
 }
 
