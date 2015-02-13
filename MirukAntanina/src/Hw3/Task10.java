10608a8aeaa0ed5f011b5182970fb4dac75008b5
Tonusha <Sidtonya@yandex.ru>
2015-02-13 14:31:45 +0300

    Task10

--- MirukAntanina/src/Hw3/Task10.java
+++ MirukAntanina/src/Hw3/Task10.java
@@ -1,18 +1,21 @@
 package Hw3;
 
-/**
- * Created by miruk on 2/11/15.
- */
+import java.util.Scanner;
+
 public class Task10 {
     public static void main(String[] args) {
-        int number = 7;
-        int simple = 0;
-        for (int i = 1; i <= number; i++) {
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        sc.close();
+        int simple = 2;
+        for (int i = 2; i <= Math.pow(number, 0.5); i++) {
             if (number % i == 0) {
                 simple++;
+                break;
             }
         }
-        if (simple == 2) {
+        if ((simple == 2) || (number == 1)) {
             System.out.println("Число " + number + " является простым.");
         } else {
             System.out.println("Число " + number + " не является простым.");
