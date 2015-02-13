b8d56fbcdeb55756266a46f93c229cede6081236
Tonusha <Sidtonya@yandex.ru>
2015-02-13 15:10:52 +0300

    Task_dop1

--- MirukAntanina/src/Hw4/Task_dop1.java
+++ MirukAntanina/src/Hw4/Task_dop1.java
@@ -7,10 +7,9 @@ import java.util.Scanner;
  */
 public class Task_dop1 {
     public static void main(String[] args) {
-        float number;
         System.out.print("Введите дробное число: ");
         Scanner sc = new Scanner(System.in);
-        number = sc.nextFloat();
+        float number = sc.nextFloat();
         number *= 1000;
         float first = number % 10;
         float two = (number % 100 - first) / 10;
