97fd312e20ca3133a015b7ea6edb66ad9f338d65
Tonusha <Sidtonya@yandex.ru>
2015-02-13 12:50:37 +0300

    Task4

--- MirukAntanina/src/Hw2/Task4.java
+++ MirukAntanina/src/Hw2/Task4.java
@@ -1,14 +1,21 @@
 package Hw2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] arg) {
-        int a = 2, b = 4, r = 6;
+        System.out.print("Введите стороны прямоугольника: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.print("Введите радиус круглой картонки: ");
+        int r = sc.nextInt();
+        sc.close();
         float c = a * a + b * b;
         double d = Math.pow(c, 0.5) / 2;
         if (r >= d) {
-            System.out.println("Прямоугольное отверстие с размерами " + a + "x" + b + " можно накрыть круглой картонкой радиусом " + r);
+            System.out.println("Прямоугольное отверстие можно накрыть круглой картонкой");
         } else {
-            System.out.println("Прямоугольное отверстие с размерами " + a + "x" + b + " нельзя накрыть круглой картонкой радиусом " + r);
+            System.out.println("Прямоугольное отверстие нельзя накрыть круглой картонкой");
         }
     }
 }
