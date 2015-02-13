f6674ef3ba9e35d078415520c3d7fae6d640822d
Tonusha <Sidtonya@yandex.ru>
2015-02-13 16:52:45 +0300

    Calculation

---
+++ MirukAntanina/src/Hw5/Сalculation.java
@@ -0,0 +1,21 @@
+package Hw5;
+
+/**
+ * Created by miruk on 2/13/15.
+ */
+public class Сalculation {
+    public Float getSquare(float a) {
+        float S = 6*a*a;
+        return S;
+    }
+
+    public Float getVolume(float a) {
+        float V = a*a*a;
+        return V;
+    }
+
+    public String getColor(Kub kub){
+        return kub.color = "Зеленый";
+    }
+
+}
