b77a87c49d1a4b0c50b3286ad1fd0f7cc27596bd
Tonya <Sidtonya@yandex.ru>
2015-02-15 19:08:56 +0300

    Task6

--- MirukAntanina/src/Hw3/Task6.java
+++ MirukAntanina/src/Hw3/Task6.java
@@ -1,25 +1,15 @@
 package Hw3;
 
-/**
- * Created by miruk on 2/11/15.
- */
 public class Task6 {
     public static void main(String[] args) {
-        int array[] = new int[20];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 10 + 1);
-            System.out.print(array[i]);
+        int n = 123456;
+        System.out.println(n);
+        int res = 0;
+        for (; n>0;){
+            res *=10;
+            res +=  n%10;
+            n/=10;
         }
-        System.out.println();
-        int temp = 0;
-        int sizeArray = array.length - 1;
-        for (int i = 0; i < array.length / 2; i++) {
-            temp = array[i];
-            array[i] = array[sizeArray - i];
-            array[sizeArray - i] = temp;
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-        }
+        System.out.println(res);
     }
 }
