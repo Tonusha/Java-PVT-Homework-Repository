64deb0cb640e24b8715fb5c24ae95feaef72ac42
Tonya <Sidtonya@yandex.ru>
2015-02-15 19:08:23 +0300

    Task_dop1

--- MirukAntanina/src/Hw4/Task_dop1.java
+++ MirukAntanina/src/Hw4/Task_dop1.java
@@ -2,25 +2,27 @@ package Hw4;
 
 import java.util.Scanner;
 
-/**
- * Created by miruk on 2/11/15.
- */
 public class Task_dop1 {
     public static void main(String[] args) {
         System.out.print("Введите дробное число: ");
         Scanner sc = new Scanner(System.in);
         float number = sc.nextFloat();
-        System.out.print("Введите сколько чисел после запятой сложить: ");
-        float n = sc.nextFloat();
-        int sum = 0, pow = 10;
-        System.out.println(number % 10);
-        for (int i=0; i<=n; i++) {
-            number*=pow;
-            float u = (number % 100-number % 10)/10;
-            System.out.println(u);
-           sum += u;
+        int res = 0;
+        int x = (int)number;
+        double drob = number - (float)x;
+        if (drob == 0.0){
+            System.out.println("У этого числа нет дробной части!");
+            return;
         }
-        System.out.println("Сумма первых трех цифр дробной части заданного положительного вещественного числа " + sum);
+        System.out.print("Введите сколько чисел после запятой сложить: ");
+        int n = sc.nextInt();
+        for (int i = 0; i<n;i++) {
+            drob *= 10;
+            x = (int)drob;
+            res += x;
+            drob -= (float)x;
+        }
+        System.out.println("Сумма первых "+n+" цифр дробной части заданного положительного вещественного числа " + res);
     }
 }
 
