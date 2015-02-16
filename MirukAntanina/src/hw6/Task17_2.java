d904d6ecf028eaf7885f9e8298241f655c69ff56
Tonusha <Sidtonya@yandex.ru>
2015-02-16 11:16:31 +0300

    Task17_2

--- MirukAntanina/src/hw6/Task17_2.java
+++ MirukAntanina/src/hw6/Task17_2.java
@@ -1,21 +1,18 @@
 package hw6;
 
-/**
- * Created by Tonya on 15.02.2015.
- */
 public class Task17_2 {
     public static void main(String[] args) {
         System.out.println("Исследуемая строка:");
         String str = "  Суть               проекта “Центр славянских языков и культур”          ";
         System.out.println(str);
         System.out.println();
-        int word_cntr = 0;//счетчик слов
+        int n = 0;
         int index = 0;
-        while(index != -1){//пока еще что-то находится
-            index = str.indexOf(' ',index+1);//ищем пробел
+        while(index != -1){
+            index = str.indexOf(' ',index+1);
             if (str.indexOf(' ',index+1)-1!=index&&index != -1)
-                word_cntr++;//чтобы исключить поля пробелов
+                n++;
         }
-        System.out.println("В этой строке "+ word_cntr+" слов");
+        System.out.println("В этой строке "+ n+" слов");
     }
 }
