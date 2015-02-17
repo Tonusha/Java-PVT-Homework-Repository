d81e20dc603ecf3bd870112637519e392bef446f
Tonya <Sidtonya@yandex.ru>
2015-02-17 21:23:23 +0300

    Task21

--- MirukAntanina/src/homeWork7/Task21.java
+++ MirukAntanina/src/homeWork7/Task21.java
@@ -3,9 +3,6 @@ package homeWork7;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by Tonya on 17.02.2015.
- */
 public class Task21 {
     public static void main(String[] args) {
         String str = "<p > </p> <p dsf;ldkfgjajhadlfgk kfj;sdf > </p> <p dfj;ksajfd;lkjsd> <b></b></p>";
