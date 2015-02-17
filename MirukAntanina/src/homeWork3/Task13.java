ea6a0e4d7f4d2acc9313deccb5ba4c9fdde30eb2
Tonusha <Sidtonya@yandex.ru>
2015-02-17 12:41:04 +0300

    Task13

--- MirukAntanina/src/homeWork3/Task13.java
+++ MirukAntanina/src/homeWork3/Task13.java
@@ -5,19 +5,13 @@ public class Task13 {
         int array[][] = new int[5][5];
         System.out.println("Массив: ");
         int size = array.length - 1;
-        int k = 0;
-        for (int i = 0; i <= size; i++) {
-            for (int j = 0; j <= size; j++) {
-                array[i][j] = (int) (Math.random() * 10);
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.println();
-        }
+        ArrayOperation.arrayCreatePrint(array);
+
 
         for (int i = 0; i <= size / 2; i++) {
-            for (int j = i; j <= size - i - 1; j++) {
+            for (int j = i; j <= size - i -1; j++) {
                 int temp = array[i][j];
-                array[i][j] = array[size - k][i];
+                array[i][j] = array[size - j][i];
                 array[size - j][i] = array[size - i][size - j];
                 array[size - i][size - j] = array[j][size - i];
                 array[j][size - i] = temp;
@@ -25,12 +19,7 @@ public class Task13 {
         }
 
         System.out.println("Массив, повернутый по часовой стрелке: ");
-        for (int i = 0; i <= size; i++) {
-            for (int j = 0; j <= size; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.println();
-        }
+        ArrayOperation.arrayPrint(array);
     }
 }
 
