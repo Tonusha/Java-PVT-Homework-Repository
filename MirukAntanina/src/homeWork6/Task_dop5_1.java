4c1c13ca410f060a6caa329991fa733d9aa6d59c
Tonusha <Sidtonya@yandex.ru>
2015-02-17 15:45:28 +0300

    Task_dop5_1

--- MirukAntanina/src/homeWork6/Task_dop5_1.java
+++ MirukAntanina/src/homeWork6/Task_dop5_1.java
@@ -1,7 +1,22 @@
 package homeWork6;
 
-/**
- * Created by miruk on 2/17/15.
- */
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
 public class Task_dop5_1 {
+    public static void main(String[] args) {
+        System.out.print("Введите три числа: ");
+        Scanner sc = new Scanner(System.in);
+        float x = sc.nextFloat();
+        float t = sc.nextFloat();
+        float s = sc.nextFloat();
+        sc.close();
+        if (1 + pow(x, s) == 0) {
+            System.out.println("Знаменатель равен 0");
+        } else {
+            double y = pow(sin(pow(x, t)), 2) / (sqrt(1 + pow(x, s)));
+            System.out.println("y = " + y);
+        }
+    }
 }
